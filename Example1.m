f35518689e6453af5a75a3c748735303c3899aa5
Ethan Puerto <epuerto96@gmail.com>
2018-04-26 13:52:40 -0400

    Finished Example 1

--- Example1.m
+++ Example1.m
@@ -1,9 +1,17 @@
-x = normrnd(0,1,100,1)
-hist(x)
-mean(x)
-std(x)
-size(x,1)
-hist(x(x<1))
-sum(x<1)
-sum(x<1)/size(x,1)
-normcdf(1,0,1)
+x = normrnd(0,1,100,1);
+hist(x);
+mean(x);
+std(x);
+size(x,1);
+hist(x(x<1));
+sum(x<1);
+sum(x<1)/size(x,1);
+normcdf(1,0,1);
+
+
+disp('Standard Error')
+SEM = std(x)/sqrt(length(x))              % Standard Error
+disp('T-Score')
+ts = tinv([0.025  0.975],length(x)-1)      % T-Score
+disp('Confidence Interval')
+CI = mean(x) + ts*SEM                      % Confidence Intervals
